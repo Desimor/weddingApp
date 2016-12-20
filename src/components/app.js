7c2d90c6cf215b386c785fa5256d75b7b7014507
Matthew Morais <moraismatthew@yahoo.com>
2016-12-20 01:31:25 -0500

    Import Axios

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import axios from 'axios'
 
 export default class App extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      text: "Initial", 
+    };
+  }
   render () {
     return (
       <div>
-        <h1>Change me</h1>
+        <h1>Wedding App</h1>
+        <p>{this.state.text}</p>
       </div>
     )
   }
